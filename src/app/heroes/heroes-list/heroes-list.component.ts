6aee4b18b16c91338d5b36a68abea3f61b676a3d
Mathieu <null>
2021-12-20 18:14:48 +0100

    animations

---
+++ src/app/heroes/heroes-list/heroes-list.component.ts
@@ -0,0 +1,31 @@
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes-list',
+  templateUrl: './heroes-list.component.html',
+  styleUrls: ['./heroes-list.component.css']
+})
+export class HeroListComponent implements OnInit {
+  heroes$!: Observable<Hero[]>;
+  selectedId = 0;
+
+  constructor(
+    private service: HeroService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.heroes$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        this.selectedId = parseInt(params.get('id')!, 10);
+        return this.service.getHeroes();
+      })
+    );
+  }
+}
